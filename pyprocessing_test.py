43dafbe35f97040675ddb035a9e54d59f0c68848
Mark Liffiton <liffiton@gmail.com>
2014-03-21 11:57:32 -0500

    Monkeypatching to make pyprocessing work outside of __main__...

--- pyprocessing_test.py
+++ pyprocessing_test.py
@@ -2,28 +2,34 @@ import multiprocessing
 import random
 import time
 
+from pyprocessing import *
+
 
 def dostuff():
+    '''
+    To get pyprocessing to work somewhere other than the real __main__ module,
+    we have to create our functions and insert them into the __main__ module by
+    importing it and monkeypatching it.
+    '''
     def draw():
-        pyp.rect(100,100,20,random.random(100))
-        pyp.ellipse(100,70,60,60)
-        pyp.ellipse(81,70,16,32)
-        pyp.ellipse(119,70,16,32)
-        pyp.line(90,150,80,160)
-        pyp.line(110,150,120,160)
-    __main__ = dostuff  # HACK
-    import pyprocessing as pyp
-    pyp.size(200,200)
-    pyp.rectMode(pyp.CENTER)
-    dostuff.draw()
-    pyp.run()
+        rect(100,100,20,random.randint(90,110))
+        ellipse(100,70,60,60)
+        ellipse(81,70,16,32)
+        ellipse(119,70,16,32)
+        line(90,150,80,160)
+        line(110,150,120,160)
+    import __main__       # HACK
+    __main__.draw = draw  # HACK  (monkeypatching)
+    size(200,200)
+    rectMode(CENTER)
+    run()
 
 
 def main():
     p = multiprocessing.Process(target=dostuff)
     p.start()
     print "Still here!"
-    time.sleep(10)
+    time.sleep(5)
     print "Still still here!"
 
 
