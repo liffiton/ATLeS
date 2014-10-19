9ff1b96058f144fdd52510828de1103a33700ca7
Mark Liffiton <liffiton@gmail.com>
2014-10-19 01:45:20 -0500

    Refactoring for encapsulation, reusable class.

--- analyze.py
+++ analyze.py
@@ -9,7 +9,141 @@ _freeze_max_speed = 0.3  # maximum speed to still consider a "freeze"
 
 
 class Grapher(object):
-    def __init__(self, time, theta, speed, valid, lost, missing, frozen, in_top, x, y, numpts):
+    def load(self, infile):
+        time, status, x, y, numpts = np.loadtxt(
+            infile,
+            delimiter=',',
+            unpack=True,
+            dtype={'names': ('time','status','x','y','numpts'), 'formats': ('f','S16','f','f','d')}
+        )
+
+        # calculate derivatives and other derived values
+        dx = np.gradient(x)
+        dy = np.gradient(y)
+        dt = np.gradient(time)
+
+        if np.min(dt) < 0:
+            print("\n[31mWARNING:[m Time travel detected!\n[31mNegative time deltas in following indices:[m "
+                  + " ".join(str(x) for x in np.where(dt < 0))
+                  + "\n"
+                  )
+
+        movement = np.matrix([dx,dy])
+        try:
+            dist = np.linalg.norm(movement, axis=0)
+        except TypeError:
+            # older version of numpy w/o axis argument
+            dist = map(np.linalg.norm, np.transpose(movement))
+        speed = dist / dt
+        theta = np.arctan2(dy, dx)  # reversed params are correct for numpy.arctan2
+        #dtheta = np.gradient(theta)
+        #angular_velocity = dtheta / dt
+
+        # produce boolean arrays
+        valid = (status != 'lost') & (np.roll(status, 1) != 'lost')  # valid if this index *and* previous are both not 'lost'
+        valid_count = np.sum(valid)
+        lost = status == 'lost'
+        missing = status == 'missing'
+        in_top = valid & (y > 0.5)
+        frozen = valid & (speed < _freeze_max_speed)
+
+        # initialize counters, etc.
+        last_entry = None
+        first_entry = None
+        entry_count = 0
+
+        time_in_top = 0
+        time_in_bottom = 0
+
+        distance_in_top = 0
+        distance_in_bottom = 0
+
+        cur_freeze_time = 0
+        total_freeze_time = 0
+        freeze_count = 0
+
+        # TODO:  cur_turn_time = 0
+
+        distance_total = 0
+        time_total_valid = 0
+
+        for i in xrange(1, len(time)):
+            if not valid[i]:
+                continue
+
+            distance_total += dist[i]
+            time_total_valid += dt[i]
+
+            # count entries to top
+            if in_top[i] and not in_top[i-1] and (last_entry is None or time[i] - last_entry > _entry_wait):
+                if first_entry is None:
+                    first_entry = time[i]
+                last_entry = time[i]
+                entry_count += 1
+
+            # sum time and distance in top/bottom
+            if in_top[i]:
+                time_in_top += dt[i]
+                distance_in_top += dist[i]
+            else:
+                time_in_bottom += dt[i]
+                distance_in_bottom += dist[i]
+
+            # count freezes
+            # "Freeze" = speed < _freeze_max_speed for at least _freeze_min_time seconds
+            if frozen[i]:
+                if not frozen[i-1]:
+                    # start of a freeze
+                    cur_freeze_time = 0
+                    freeze_count += 1
+                total_freeze_time += dt[i]
+                cur_freeze_time += dt[i]
+            elif frozen[i-1]:
+                # end of a freeze
+                if cur_freeze_time < _freeze_min_time:
+                    # don't count this one
+                    freeze_count -= 1
+                    total_freeze_time -= cur_freeze_time
+
+            # count erratic movements
+            # "Erratic movement" = a string of at least _erratic_count 'turns' in less than _erratic_time seconds
+            # "Turn" = abs(angular_velocity) > _turn_vel for at least _turn_time seconds
+            # TODO
+
+        print "#Datapoints: %d" % len(valid)
+        print "#Valid: %d" % valid_count
+        print "%%Valid datapoints: %0.3f" % (valid_count / float(len(valid)))
+        print "Total time: %0.2f seconds" % time[-1]
+        print "Valid time: %0.2f seconds" % time_total_valid
+        print
+        print "Total distance traveled: %0.2f [units]" % (distance_total)
+        print "Average velocity: %0.3f [units]/second" % (distance_total / time_total_valid)
+        print
+        print "#Entries to top: %d" % entry_count
+        if entry_count:
+            print "Time of first entry: %0.2f seconds" % (first_entry)
+        print
+        print "Time in top:    %0.2f seconds" % time_in_top
+        print "Time in bottom: %0.2f seconds" % time_in_bottom
+        if time_in_bottom:
+            print "Top/bottom time ratio: %0.3f" % (time_in_top/time_in_bottom)
+        if entry_count:
+            print "Avg. time per entry: %0.2f seconds" % (time_in_top / entry_count)
+        print
+        print "Distance in top:    %0.3f [units]" % distance_in_top
+        print "Distance in bottom: %0.3f [units]" % distance_in_bottom
+        if distance_in_bottom:
+            print "Top/bottom distance ratio: %0.3f" % (distance_in_top/distance_in_bottom)
+        if entry_count:
+            print "Avg. distance per entry: %0.3f [units]" % (distance_in_top / entry_count)
+        print
+        print "#Freezes: %d" % freeze_count
+        if freeze_count:
+            print "Total time frozen: %0.3f seconds" % total_freeze_time
+            print "Avg. time per freeze: %0.3f seconds" % (total_freeze_time / freeze_count)
+            print "Freeze frequency: %0.2f per minute" % (60.0*(freeze_count / time_total_valid))
+
+        # setup internal data
         self._len = len(time)
         self._time = time
         self._theta = theta
@@ -76,6 +210,8 @@ class Grapher(object):
         return (r,g,b, 0.5)
 
     def plot(self):
+        plt.clf()
+
         maxpts = 500
         numplots = 1 + self._len / maxpts
 
@@ -126,7 +262,8 @@ class Grapher(object):
         self._set_foregroundcolor(legend_ax, '0.6')
 
     def plot_heatmap(self, numplots=1):
-        plt.figure(numplots)
+        plt.clf()
+        plt.figure(numplots, (4, 4*numplots))
         for i in range(numplots):
             start = i * len(self._x) / numplots
             end = (i+1) * len(self._x) / numplots
@@ -218,6 +355,19 @@ class Grapher(object):
             headlength=0, headwidth=0, headaxislength=0     # no arrowheads
         )
 
+    def format(self):
+        # Format nicely
+        plt.gcf().patch.set_facecolor('0.1')
+        plt.tight_layout()
+
+    def show(self):
+        self.format()
+        plt.show()
+
+    def savefig(self, outfile):
+        self.format()
+        plt.savefig(outfile, facecolor=plt.gcf().get_facecolor(), edgecolor='none')
+
 
 def main():
     parser = argparse.ArgumentParser(description='Analyze zebrafish Skinner box experiment logs.')
@@ -227,154 +377,18 @@ def main():
     parser.add_argument('--heat-num', type=int, default=1)
     args = parser.parse_args()
 
-    time, status, x, y, numpts = np.loadtxt(
-        args.infile,
-        delimiter=',',
-        unpack=True,
-        dtype={'names': ('time','status','x','y','numpts'), 'formats': ('f','S16','f','f','d')}
-    )
-
-    # calculate derivatives and other derived values
-    dx = np.gradient(x)
-    dy = np.gradient(y)
-    dt = np.gradient(time)
-
-    if np.min(dt) < 0:
-        print("\n[31mWARNING:[m Time travel detected!\n[31mNegative time deltas in following indices:[m "
-              + " ".join(str(x) for x in np.where(dt < 0))
-              + "\n"
-              )
-
-    movement = np.matrix([dx,dy])
-    try:
-        dist = np.linalg.norm(movement, axis=0)
-    except TypeError:
-        # older version of numpy w/o axis argument
-        dist = map(np.linalg.norm, np.transpose(movement))
-    speed = dist / dt
-    theta = np.arctan2(dy, dx)  # reversed params are correct for numpy.arctan2
-    #dtheta = np.gradient(theta)
-    #angular_velocity = dtheta / dt
-
-    # produce boolean arrays
-    valid = (status != 'lost') & (np.roll(status, 1) != 'lost')  # valid if this index *and* previous are both not 'lost'
-    valid_count = np.sum(valid)
-    lost = status == 'lost'
-    missing = status == 'missing'
-    in_top = valid & (y > 0.5)
-    frozen = valid & (speed < _freeze_max_speed)
-
-    # initialize counters, etc.
-    last_entry = None
-    first_entry = None
-    entry_count = 0
-
-    time_in_top = 0
-    time_in_bottom = 0
-
-    distance_in_top = 0
-    distance_in_bottom = 0
-
-    cur_freeze_time = 0
-    total_freeze_time = 0
-    freeze_count = 0
-
-    # TODO:  cur_turn_time = 0
-
-    distance_total = 0
-    time_total_valid = 0
-
-    for i in xrange(1, len(time)):
-        if not valid[i]:
-            continue
-
-        distance_total += dist[i]
-        time_total_valid += dt[i]
-
-        # count entries to top
-        if in_top[i] and not in_top[i-1] and (last_entry is None or time[i] - last_entry > _entry_wait):
-            if first_entry is None:
-                first_entry = time[i]
-            last_entry = time[i]
-            entry_count += 1
-
-        # sum time and distance in top/bottom
-        if in_top[i]:
-            time_in_top += dt[i]
-            distance_in_top += dist[i]
-        else:
-            time_in_bottom += dt[i]
-            distance_in_bottom += dist[i]
-
-        # count freezes
-        # "Freeze" = speed < _freeze_max_speed for at least _freeze_min_time seconds
-        if frozen[i]:
-            if not frozen[i-1]:
-                # start of a freeze
-                cur_freeze_time = 0
-                freeze_count += 1
-            total_freeze_time += dt[i]
-            cur_freeze_time += dt[i]
-        elif frozen[i-1]:
-            # end of a freeze
-            if cur_freeze_time < _freeze_min_time:
-                # don't count this one
-                freeze_count -= 1
-                total_freeze_time -= cur_freeze_time
-
-        # count erratic movements
-        # "Erratic movement" = a string of at least _erratic_count 'turns' in less than _erratic_time seconds
-        # "Turn" = abs(angular_velocity) > _turn_vel for at least _turn_time seconds
-        # TODO
-
-    print "#Datapoints: %d" % len(valid)
-    print "#Valid: %d" % valid_count
-    print "%%Valid datapoints: %0.3f" % (valid_count / float(len(valid)))
-    print "Total time: %0.2f seconds" % time[-1]
-    print "Valid time: %0.2f seconds" % time_total_valid
-    print
-    print "Total distance traveled: %0.2f [units]" % (distance_total)
-    print "Average velocity: %0.3f [units]/second" % (distance_total / time_total_valid)
-    print
-    print "#Entries to top: %d" % entry_count
-    if entry_count:
-        print "Time of first entry: %0.2f seconds" % (first_entry)
-    print
-    print "Time in top:    %0.2f seconds" % time_in_top
-    print "Time in bottom: %0.2f seconds" % time_in_bottom
-    if time_in_bottom:
-        print "Top/bottom time ratio: %0.3f" % (time_in_top/time_in_bottom)
-    if entry_count:
-        print "Avg. time per entry: %0.2f seconds" % (time_in_top / entry_count)
-    print
-    print "Distance in top:    %0.3f [units]" % distance_in_top
-    print "Distance in bottom: %0.3f [units]" % distance_in_bottom
-    if distance_in_bottom:
-        print "Top/bottom distance ratio: %0.3f" % (distance_in_top/distance_in_bottom)
-    if entry_count:
-        print "Avg. distance per entry: %0.3f [units]" % (distance_in_top / entry_count)
-    print
-    print "#Freezes: %d" % freeze_count
-    if freeze_count:
-        print "Total time frozen: %0.3f seconds" % total_freeze_time
-        print "Avg. time per freeze: %0.3f seconds" % (total_freeze_time / freeze_count)
-        print "Freeze frequency: %0.2f per minute" % (60.0*(freeze_count / time_total_valid))
-
-    g = Grapher(time, theta, np.where(valid, speed, 0), valid, lost, missing, frozen, in_top, x, y, numpts)
+    g = Grapher()
+    g.load(args.infile)
 
     if args.heat_only:
         g.plot_heatmap(args.heat_num)
     else:
         g.plot()
 
-    # Format nicely
-    plt.gcf().patch.set_facecolor('0.1')
-    #plt.tight_layout()
-
     if args.outfile is None:
-        plt.show()
+        g.show()
     else:
-        plt.savefig(args.outfile)
+        g.savefig(args.outfile)
 
 if __name__ == '__main__':
     main()
