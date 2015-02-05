a52c1669a843e8afcf629de819e8144d6832bc7b
Mark Liffiton <liffiton@gmail.com>
2015-02-05 16:06:24 +0000

    Add light PWM control to test code.

--- sensors_test.py
+++ sensors_test.py
@@ -1,8 +1,12 @@
 from TSL2561 import TSL2561
 from MCP9808 import MCP9808
 import time
+import wiringpi2 as wiringpi
+
 
 def main():
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(18,2)  # enable PWM mode on pin 18
     tsl = TSL2561(debug=0)
     mcp = MCP9808(debug=0)
     #tsl.set_gain(16)
@@ -13,8 +17,8 @@ def main():
         print("%d,%d = %d lux" % (full, ir, lux))
         temp = mcp.read_temp()
         print("%0.2f degC" % temp)
+        wiringpi.pwmWrite(18, min(int(lux), 1024))
         time.sleep(1)
-
 
 
 if __name__ == '__main__':
