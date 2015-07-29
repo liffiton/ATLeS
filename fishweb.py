8956010c4b4212b4ad9da6e8c7e4a58d6a289275
Mark Liffiton <liffiton@gmail.com>
2015-07-28 23:05:52 -0500

    Cache track data in memory for faster responses / less CPU waste.

--- fishweb.py
+++ fishweb.py
@@ -62,36 +62,51 @@ def _mkdir(path):
             raise
 
 
+def _get_track_data(track):
+    states = collections.Counter()
+    heatmap = collections.Counter()
+    with open(track) as f:
+        i = -1  # so lines = 0 if file is empty
+        for i, line in enumerate(f):
+            vals = line.split(',')
+            state, x, y = vals[1:4]
+            if state == "init":
+                state = "lost"
+            states[state] += 1
+            try:
+                if state != "lost":
+                    x = "%0.3f" % (float(x) - math.fmod(float(x), 1.0/15))
+                    y = "%0.3f" % (float(y) - math.fmod(float(y), 1.0/10))
+                    heatmap[(x,y)] += 1
+            except ValueError:
+                pass  # not super important if we can't parse x,y
+        lines = i+1
+    aml = ["%0.3f" % (states[key] / float(lines)) for key in 'acquired', 'missing', 'lost']
+    if heatmap:
+        maxheat = max(heatmap.values())
+        heat = [(key[0], key[1], str(float(value)/maxheat)) for key, value in heatmap.items()]
+    else:
+        heat = []
+
+    return lines, aml, heat
+
+
+# Cache computed a/m/l and heatmap data to avoid recomputation
+track_data_cache = {}
+
+
 @route('/')
 @view('index')
 def index():
+    global track_data_cache
     tracks = []
     for index, track in enumerate(_tracks()):
-        states = collections.Counter()
-        heatmap = collections.Counter()
-        with open(track) as f:
-            i = -1  # so lines = 0 if file is empty
-            for i, line in enumerate(f):
-                vals = line.split(',')
-                state, x, y = vals[1:4]
-                if state == "init":
-                    state = "lost"
-                states[state] += 1
-                try:
-                    if state != "lost":
-                        x = "%0.3f" % (float(x) - math.fmod(float(x), 1.0/15))
-                        y = "%0.3f" % (float(y) - math.fmod(float(y), 1.0/10))
-                        heatmap[(x,y)] += 1
-                except ValueError:
-                    pass  # not super important if we can't parse x,y
-            lines = i+1
-        name = track.split('/')[-1]
-        aml = ["%0.3f" % (states[key] / float(lines)) for key in 'acquired', 'missing', 'lost']
-        if heatmap:
-            maxheat = max(heatmap.values())
-            heat = [(key[0], key[1], str(float(value)/maxheat)) for key, value in heatmap.items()]
+        if track in track_data_cache:
+            lines, aml, heat = track_data_cache[track]
         else:
-            heat = []
+            lines, aml, heat = _get_track_data(track)
+            track_data_cache[track] = (lines, aml, heat)
+        name = track.split('/')[-1]
         tracks.append( (index, track, lines, aml, heat, _imgs(name)) )
     return dict(tracks=tracks)
 
