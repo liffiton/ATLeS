ddc3f7ebe3428da885769cc0182864352897c792
Mark Liffiton <liffiton@gmail.com>
2015-10-28 00:40:05 -0500

    Remove old testing code.

--- fishyclient.py
+++
@@ -1,9 +0,0 @@
-# fishyclient.py -- a test(?) client that connects to fishymiddle.py
-
-import rpyc
-import random
-
-if __name__ == '__main__':
-    c = rpyc.connect("localhost", 12344)
-    print c.root
-    c.root.register("MYNAMEIS%d" % random.randint(0, 100))
