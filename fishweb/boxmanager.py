f9c16a812913ee1f457a396c0b3014fc6c1d6fb9
Mark Liffiton <liffiton@gmail.com>
2016-01-01 02:32:18 -0600

    Run part of sync over existing SSH tunnel, not new one

--- fishweb/boxmanager.py
+++ fishweb/boxmanager.py
@@ -61,12 +61,13 @@ class Box(object):
         try:
             # Copy remote files into an archive dir, then have rsync
             # delete the originals after the transfer
-            cmd = ['ssh', '%s@%s' % (self.user, self.ip), 'cp -r %s %s' % (self.trackdir, self.archivedir)]
-            subprocess.check_output(cmd)
-            # Currently does *not* copy the debugframes (following 2 lines are
-            # commented), so they will be removed from remote entirely.
-            #cmd = ['ssh', '%s@%s' % (self.user, self.ip), 'cp -r %s %s' % (self.dbgramedir, self.archivedir)]
+            self._tunnel["cp -r %s %s" % (self.trackdir, self.archivedir)]
+            #cmd = ['ssh', '%s@%s' % (self.user, self.ip), 'cp -r %s %s' % (self.trackdir, self.archivedir)]
             #subprocess.check_output(cmd)
+            ## Currently does *not* copy the debugframes (following 2 lines are
+            ## commented), so they will be removed from remote entirely.
+            ##cmd = ['ssh', '%s@%s' % (self.user, self.ip), 'cp -r %s %s' % (self.dbgramedir, self.archivedir)]
+            ##subprocess.check_output(cmd)
 
             # Both source and dest must end with / to copy contents of one folder
             # into another, isntead of copying the source folder into the
