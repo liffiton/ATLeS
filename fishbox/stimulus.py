3d281fd4e1268b7e0429139d0445baf64bfa238d
Mark Liffiton <liffiton@gmail.com>
2016-05-19 09:50:06 -0500

    Separation of electrical and light stimulus code.

--- fishbox/stimulus.py
+++ fishbox/stimulus.py
@@ -8,7 +8,8 @@ import sys
 import wiring
 
 
-_LIGHT_PWM_PIN = 18  # pin for PWM control of visible light bar
+_LIGHT_PWM_PIN = 18      # pin for PWM control of visible light bar
+_ELECTRIC_STIM_PIN = 18  # pin for control of the electric current stimulation
 
 
 class NotRootError(RuntimeError):
@@ -82,52 +83,53 @@ class DummyStimulus(StimulusBase):
             self._stimcount += 1
 
 
-class LightBarStimulus(ThreadedStimulus):
-    def __init__(self, nostim_level, stim_level, freq_Hz=None):
-        '''Initialize as a ThreadedStimulus with LightBarHelper helper.
-        freq_Hz=0 means the light bar will be on for as long as the
+class GPIOStimulus(ThreadedStimulus):
+    def __init__(self, nostim_level, stim_level, pin, freq_Hz=None):
+        '''Initialize as a ThreadedStimulus with GPIOHelper helper.
+        freq_Hz=0 means the pin will be set to stim_level for as long as the
         stimulus is active.
         '''
-        helper = LightBarHelper(nostim_level, stim_level, freq_Hz)
-        super(LightBarStimulus, self).__init__(helper)
+        helper = GPIOHelper(nostim_level, stim_level, pin, freq_Hz)
+        super(GPIOStimulus, self).__init__(helper)
 
 
-class LightBarHelper(object):
-    '''Stimulus in the form of flashing the visible light LED bar at a given frequency.'''
-    def __init__(self, nostim_level, stim_level, freq_Hz=None):
+class GPIOHelper(object):
+    '''Stimulus in the form of activating or toggling a GPIO pin at a given frequency.'''
+    def __init__(self, nostim_level, stim_level, pin, freq_Hz=None):
         self._stim_on = False   # is stimulus activated?
-        self._on = False        # is light bar on?
+        self._on = False        # is stimulus pin currently on? (may be off while 'activated' if toggling)
 
-        self._nostim_level = nostim_level  # PWM value for light level when stimulus not active
-        self._stim_level = stim_level      # PWM value for light level when stimulus is active
+        self._nostim_level = nostim_level  # PWM value for pin when stimulus not active
+        self._stim_level = stim_level      # PWM value for pin when stimulus is active
+        self._pin = pin                    # GPIO pin to control
         if freq_Hz is None or freq_Hz == 0:
             self._interval = None  # infinite wait in poll()
         else:
             self._interval = 1.0 / freq_Hz / 2  # half of the period
 
-        self._lightval = None  # stores current PWM value to avoid extraneous pwmWrites
+        self._pinval = None  # stores current PWM value to avoid extraneous pwmWrites
 
         # must be root to access GPIO, and wiringpi itself crashes in a way that
         # leaves the camera (setup in tracking.py) inaccessible until reboot.
         if (not wiring.wiring_mocked) and (os.geteuid() != 0):
             raise NotRootError("%s must be run with sudo." % sys.argv[0])
 
-    def _set_light(self, val):
-        if self._lightval != val:
-            wiring.pwm(_LIGHT_PWM_PIN, val)
-            self._lightval = val
+    def _set_pin(self, val):
+        if self._pinval != val:
+            wiring.pwm(self._pin, val)
+            self._pinval = val
 
-    def _light_off(self):
-        self._set_light(0)
+    def _pin_off(self):
+        self._set_pin(0)
 
-    def _light_on(self):
-        self._set_light(1023)
+    def _pin_on(self):
+        self._set_pin(1023)
 
-    def _light_nostim(self):
-        self._set_light(self._nostim_level)
+    def _pin_nostim(self):
+        self._set_pin(self._nostim_level)
 
-    def _light_stim(self):
-        self._set_light(self._stim_level)
+    def _pin_stim(self):
+        self._set_pin(self._stim_level)
 
     def _handle_command(self, cmd):
         if cmd:
@@ -139,22 +141,22 @@ class LightBarHelper(object):
     def _update(self):
         if self._stim_on:
             if self._interval is None:
-                self._light_stim()
+                self._pin_stim()
             else:
                 # toggle fully on/off
                 self._on = not self._on
                 if self._on:
-                    self._light_on()
+                    self._pin_on()
                 else:
-                    self._light_off()
+                    self._pin_off()
         else:
-            self._light_nostim()
+            self._pin_nostim()
 
     def _begin(self):
-        self._light_nostim()
+        self._pin_nostim()
 
     def _end(self):
-        self._light_off()
+        self._pin_off()
 
     def stimulus_thread(self, pipe):
         # ignore signals that will be handled by parent
@@ -178,3 +180,14 @@ class LightBarHelper(object):
                 msg_ready = pipe.poll()  # no timeout, return immediately inside loop
 
             self._update()
+
+
+class LightBarStimulus(GPIOStimulus):
+    def __init__(self, nostim_level, stim_level, freq_Hz=None):
+        '''freq_Hz=0 means the light bar will be on for as long as the stimulus is active.'''
+        super(LightBarStimulus, self).__init__(nostim_level, stim_level, _LIGHT_PWM_PIN, freq_Hz)
+
+
+class ElectricalStimulus(GPIOStimulus):
+    def __init__(self):
+        super(ElectricalStimulus, self).__init__(0, 1023, _ELECTRIC_STIM_PIN, 0)
