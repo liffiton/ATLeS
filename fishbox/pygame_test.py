5bf27657543b88839c8d11c204323960f6a1910a
Mark Liffiton <liffiton@gmail.com>
2016-07-26 14:50:17 -0500

    Newer pygame test.

--- fishbox/pygame_test.py
+++ fishbox/pygame_test.py
@@ -1,56 +1,62 @@
 import os
 import pygame
 import time
-import random
- 
-class pyscope :
-	screen = None;
-	
-	def __init__(self):
-		"Ininitializes a new pygame screen using the framebuffer"
-		# Based on "Python GUI in Linux frame buffer"
-		# http://www.karoltomala.com/blog/?p=679
-		disp_no = os.getenv("DISPLAY")
-		if disp_no:
-			print "I'm running under X display = {0}".format(disp_no)
-		
-		driver = 'directfb'   # alternatives: 'fbcon', 'svgalib'
-		# Make sure that SDL_VIDEODRIVER is set
-		if not os.getenv('SDL_VIDEODRIVER'):
-			os.putenv('SDL_VIDEODRIVER', driver)
-		try:
-			pygame.display.init()
-		except pygame.error:
-			raise Exception('Driver: {0} failed.'.format(driver))
-	
-		size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-		print "Framebuffer size: %d x %d" % (size[0], size[1])
-		self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN | pygame.DOUBLEBUF)
-		# Clear the screen to start
-		self.screen.fill((0, 0, 0))        
-		pygame.mouse.set_visible(False)
-		# Initialise font support
-		#pygame.font.init()
-		# Render the screen
-		pygame.display.update()
- 
-	def __del__(self):
-		"Destructor to make sure pygame shuts down, etc."
- 
-	def test(self):
-		import time
-		x = time.time()
-		for i in range(100):
-			# Fill the screen with red (255, 0, 0)
-			red = (255, 0, 0)
-			self.screen.fill(red)
-			# Update the display
-			pygame.display.flip()
-			self.screen.fill((0,0,0))
-			pygame.display.flip()
 
-		print time.time()-x
- 
-# Create an instance of the PyScope class
-scope = pyscope()
-scope.test()
+
+def get_screen(xwin=False):
+    """Initializes a new pygame screen using the framebuffer"""
+    # Based on "Python GUI in Linux frame buffer"
+    # http://www.karoltomala.com/blog/?p=679
+    disp_no = os.getenv("DISPLAY")
+    if disp_no:
+        print "I'm running under X display = {0}".format(disp_no)
+
+    # Make sure that SDL_VIDEODRIVER is set
+    if xwin:
+        driver = 'x11'
+    else:
+        driver = 'directfb'   # alternatives: 'fbcon', 'svgalib'
+    if not os.getenv('SDL_VIDEODRIVER'):
+        os.putenv('SDL_VIDEODRIVER', driver)
+
+    try:
+        pygame.display.init()
+    except pygame.error:
+        raise Exception('Display init failed with driver: {0}'.format(driver))
+
+    if xwin:
+        size = (320, 200)
+        options = 0
+    else:
+        # fullscreen
+        info = pygame.display.Info()
+        size = (info.current_w, info.current_h)
+        print "Framebuffer size: %d x %d" % (size[0], size[1])
+        options = pygame.FULLSCREEN | pygame.DOUBLEBUF
+    screen = pygame.display.set_mode(size, options)
+
+    # Clear the screen to start
+    screen.fill((0, 0, 0))
+    pygame.mouse.set_visible(False)
+    # Render the screen
+    pygame.display.update()
+
+    return screen
+
+
+def test(screen):
+    img = pygame.image.load('zebrafish.png')
+    imgscaled = pygame.transform.smoothscale(img, screen.get_size())
+    x = time.time()
+    for i in range(100):
+        screen.blit(img, (0, 0))
+        # Update the display
+        pygame.display.flip()
+        screen.blit(imgscaled, (0, 0))
+        pygame.display.flip()
+
+    print time.time()-x
+
+# Testing
+screen = get_screen(xwin=True)
+test(screen)
